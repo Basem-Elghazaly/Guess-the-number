8f43bbbecbc9f4b98411456bb795d96c325e70ab
Basem-Elghazaly <basem100tito@gmail.com>
2023-09-02 03:54:22 +0300

    Guess the number (game)

---
+++ main.js
@@ -0,0 +1,96 @@
+"use strict";
+
+/// Selecting elements
+const square = document.querySelector(".number");
+const leftSection = document.querySelector(".left");
+const rightSection = document.querySelector(".right");
+const myNumber = document.querySelector(".guess");
+const btn = document.querySelector(".btn");
+const message = document.querySelector(".message");
+const score = document.querySelector(".score");
+const labelScore = document.querySelector(".label-score");
+const highScore = document.querySelector(".label-highscore");
+const again = document.querySelector(".again");
+const bigScore = document.querySelector(".big-score");
+
+/*
+- Make a random number from 1 to 20.
+- Check the entered number if equals the random number.
+- If number is higher than the random, Execute a condition. if lower execute   
+  another condition, else execute Correct number.
+*/
+
+// Check if local storage has data or not.
+if (localStorage.number == null) {
+    bigScore.textContent = `0`;
+} else {
+    bigScore.textContent = localStorage.number;
+}
+
+// Making the random number.
+const randomNumber = Math.round(Number(Math.random() * 20));
+score.textContent = `20`;
+
+// Events
+again.addEventListener("click", () => reloadAndClear())
+btn.addEventListener("click", () => check());
+
+// The main function.
+function check() {
+    if (myNumber.value > 0 && myNumber.value < 21) {
+        if (myNumber.value > randomNumber) {
+            message.textContent = `👆 too high`;
+            decrease();
+            myNumber.focus();
+        }
+        else if (myNumber.value < randomNumber) {
+            message.textContent = `👇 too low`;
+            decrease();
+            myNumber.focus();
+        } else {
+            message.textContent = `🎁 Congratulations `
+            square.textContent = randomNumber;
+            correct();
+        }
+    }
+    clearInput();
+}
+
+// Clear the input if the answer is wrong.
+function clearInput() {
+    myNumber.value = '';
+}
+
+// Decreases by 1 when answer is wrong.
+function decrease() {
+    score.textContent -= 1;
+};
+
+// If answer is correct, This function would be executed. 
+function correct() {
+    document.body.style.backgroundColor = '#105c10'
+    bigScore.style.color = 'rgb(255, 157, 0)';
+    bigScore.style.fontWeight = 'bold';
+
+    // Check data in local storage
+    if (localStorage.number == null) {
+        localStorage.setItem("number", score.textContent)
+    }
+    if (score.textContent >= localStorage.number) {
+        localStorage.number = score.textContent;
+        bigScore.textContent = score.textContent;
+    }
+
+    // Event to play again.
+    const reload = document.querySelector(".reload");
+    reload.style.display = 'block';
+    reload.onclick = function () { location.reload() }
+}
+
+// Reload the application and clear the local storage => highscore[0].
+function reloadAndClear() {
+    location.reload();
+    localStorage.clear();
+    bigScore.textContent = `0`;
+}
+
